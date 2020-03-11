ab46c2d4c9bb1960b56c6f5b7f7dad3303e2f648
leye20 <leyeagbo@gmail.com>
2020-03-11 15:34:34 -0600

    added classes

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// import './Movies.Class.js';
+
 /**
  * Lets see what some of our newer array methods are...
  */
@@ -137,10 +139,134 @@ console.log( addNums() ); // o arguments, but our default values kick in.
  * Rest parameter.
  */
 
-findHighNum = ( ...args ) => { // ...args will capture any number of arguments that we pass. / ... means we can pass in any amount of number.
-    console.log( args ); // it can be called by the nam we had inbetween our parenthesis
-    return args;
+// Rest paramater MUST BE THE LAST PARAMETER.
+findHighNums = ( minNum = 0, ...args ) => { // ...args will capture any number of arguments that we pass!/ ... means we can pass in any amount of number.
+    minNum = Number( minNum );
+    if ( minNum === NaN ) minNum = 0;
+    const highNums = args.filter( ( element ) => element > minNum );
+    // console.log( args ); // It can be called by the name we had inbetween our parenthesis.
+    return highNums;
 }
 
+console.log( findHighNums( 5, 2, 20, 50, 3, 0, 10, 24, -508 ) );
+
+// Getting the sum of ALL numbers passed in!
 addAllNums = ( ...nums ) => nums.reduce( ( a, v ) => a + v );
-console.log( addAllNums( 64, 6, 10 ) );
+console.log( addAllNums( 64, 6, 10, 20 ) );
+
+/**
+ * Template Literals
+ */
+
+const helloVar = 'Hello, World';
+const sampleName = 'Sam';
+
+const myNewString = `Hey there, my name is ${sampleName}; I\'d like to give you a big: "${helloVar}"!!`; // The syntax for templte literals is: ${variableName}
+// Note: This only works inside of backticks!(`) But is super useful. It saves us from extra concatenation.
+console.log( myNewString );
+
+/**
+ * Spread Operator
+ * ***Don't get this confused with the rest parameter! Both are three dots...
+ * ***Remember that rest parameter is always in PARAMETER area of a function, nowhere else. Spread is used ANYWHERE else, basically.
+ */
+
+const newArray = [ 'Hello', ',', ' ', 'World', '!', ];
+console.log( newArray );
+console.log( ...newArray ); // Brought the values in the array together a one (space-separated) string, or did it?
+
+// Spread operator separates into separate values.
+const aBunchOfNums = [ 5, 10, 36, 58 ];
+console.log( addAllNums( 5, 10, 36, 58 ) ); // This and the below, are equivalent (our array gets broken up!)
+console.log( addAllNums( ...aBunchOfNums ) ); // spreading an argument.
+
+/**
+ * String Methods
+ */
+
+/**
+ * .search() Method:
+ * 
+ * Regex Learning Tool: https://regexone.com/
+ */
+
+const myTestString = 'Greetings, Program!';
+console.log( myTestString.search( /gs/i ) ); // Regex match. i is case insensitive, which means it does not need to find the exact match. Retruns the Substring Position. TING is 4.
+
+/**
+ * .replace () Method:
+ */
+
+const myReplacedString = myTestString.replace( /Program/, 'World' ); // Find a string, and replace it! Super cool.
+console.log( myReplacedString );
+
+/**
+ * Blocks:
+ */
+
+// Unnamed block.. we can use this to contain otherwise-global const and let variables.
+{
+    const myBlockNestedVar = 38;
+}
+
+// console.log( myBlockNestedVar );
+
+let myFinalResult = 0;
+// We can name blocks if we want.
+myOrganiseName: { // This could be named anything... just a label
+    // We can organize some code in here, and control scope of let.const.
+    let myBlockNestedVar = 55; // We used the same name as in our unnamed block!? That's okay, it was born in a differently scoped block!.
+    myBlockNestedVar++;
+    console.log( myBlockNestedVar );
+    myFinalResult - myBlockNestedVar
+}
+
+console.log( myFinalResult );
+
+/**
+ * Classes.
+ * *** Check our import, thats how we're accessing this.
+ */
+
+
+class Movie {
+    constructor( name, genre, year) {
+        this.name = name;
+        this.genre = genre;
+        this.year = year;
+    }
+    get getName() { // How we retrieve the property. A GETTER.
+        return this.name;
+    }
+    set setName( name ) { // How we store a new property value. A SETTER.
+        if ( typeof name === 'string' ) {
+            this.name = name;
+        } else {
+            this.name = name.toString();
+        }
+    }
+    get getYear() {
+        return 'y'+this.year;
+    }
+    set setYear( year ) {
+        this.year = Number( year );
+    }
+    showPoster() { // This is a method! we can name them as we'd like.
+        const info = `
+            MOVIE INFO
+            ==========
+            Name: ${this.name}
+            Genre: ${this.genre}
+            Year: ${this.year}
+        `;
+        return info;
+    }
+}
+
+const tron = new Movie( 'Disney\'s TRON', 'Adventure', 1982 );
+const dragonheart = new Movie( 'DragonHeart', 'Medieval Fantasy', 1996 );
+const godfather = new Movie( 'Godfather II', 'Action/Thriller', 1974 );
+
+console.log( tron );
+console.log( dragonheart );
+console.log( godfather );
